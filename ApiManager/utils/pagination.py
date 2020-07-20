5629c876d95845b615fe891d3aa72a04ef00c938
york.yu <york.yu@ihr360.com>
2020-07-20 15:49:11 +0800

    优化搜索列表接口中创建人筛选逻辑

--- ApiManager/utils/pagination.py
+++ ApiManager/utils/pagination.py
@@ -140,8 +140,12 @@ def get_pager_info(Model, filter_query, url, id, per_items=12):
         if belong_project != 'All' and belong_module != '请选择':
             obj = obj.filter(belong_project__contains=belong_project).filter(
                 belong_module__module_name__contains=belong_module)
-            if name is not '':
+            if name is not '' and user is '':
                 obj = obj.filter(name__contains=name)
+            if name is '' and user is not '':
+                obj = obj.filter(author__contains=user)
+            if name is not '' and user is not '':
+                obj = obj.filter(name__contains=name, author__contains=user)
 
         else:
             if belong_project != 'All':
@@ -149,7 +153,13 @@ def get_pager_info(Model, filter_query, url, id, per_items=12):
             elif belong_module != '请选择':
                 obj = obj.filter(belong_module__module_name__contains=belong_module)
             else:
-                obj = obj.filter(name__contains=name) if name is not '' else obj.filter(author__contains=user)
+               # obj = obj.filter(name__contains=name) if name is not '' else obj.filter(author__contains=user)
+                if name is not '' and user is '':
+                    obj = obj.filter(name__contains=name)
+                if name is '' and user is not '':
+                    obj = obj.filter(author__contains=user)
+                if name is not '' and user is not '':
+                    obj = obj.filter(name__contains=name, author__contains=user)
 
     if url != '/api/periodictask/':
         obj = obj.order_by('-update_time')
